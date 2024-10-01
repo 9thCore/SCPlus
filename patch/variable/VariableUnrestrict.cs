10abdf646a2e07e077d9178ad0610ed414df31e3
9thCore <38700349+9thCore@users.noreply.github.com>
2024-10-01 21:30:58 +0300

    Return bogus "All disease types" value
    Various sections of code use this to determine what to do - this unlocks several options, from lifting disease type restrictions from variables, to allowing Gov. Actions for every disease type

---
+++ patch/variable/VariableUnrestrict.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SCPlus.patch.variable
+{
+    [HarmonyPatch]
+    internal class VariableUnrestrict
+    {
+        private static readonly HashSet<Disease.EDiseaseType> ALL_TYPES = 
+            new(Enum.GetValues(typeof(Disease.EDiseaseType)).Cast<Disease.EDiseaseType>());
+
+        // Return a set of all available disease types whenever code asks for available diseases
+        [HarmonyPatch(typeof(ScenarioInformation))]
+        [HarmonyPatch(nameof(ScenarioInformation.GetDiseaseTypes))]
+        [HarmonyPrefix]
+        internal static bool GetDiseaseTypesPatch(ref HashSet<Disease.EDiseaseType> __result)
+        {
+            __result = ALL_TYPES;
+            return false;
+        }
+    }
+}
