b4e68ae19836742bb68b8550959088ec0187fa0d
9thCore <38700349+9thCore@users.noreply.github.com>
2024-10-05 21:16:55 +0300

    change param order,move translation into new class
    - language is now first parameter and mandatory
    - idea is for localised text to be in its own file each

--- patch/lang/LanguageRegister.cs
+++ patch/lang/LanguageRegister.cs
@@ -1,131 +1,47 @@
-﻿using SCPlus.plugin;
+﻿using SCPlus.patch.lang.localization;
+using SCPlus.plugin;
 using System;
 
 namespace SCPlus.patch.lang
 {
     internal class LanguageRegister
     {
-        private static readonly string SCPLUS_PREFIX = "[+]";
+        internal static readonly string SCPLUS_PREFIX = "[+]";
 
         internal static void Awake()
         {
             if (Config.exposeMoreVariables.Value)
             {
-                RegisterSCPlusVariableFromExisting(
-                    "custom_global_variable_6");
-
-                RegisterSCPlusVariableFromExisting(
-                    "local_cure_research",
-                    tooltipSuffix: $"\n{SCPLUS_PREFIX} Gets reset every turn, changes will not be kept");
-
-                RegisterSCPlusVariable(
-                    "transmission_extra_cost",
-                    "Transmission extra cost",
-                    "Additional cost of Transmission tech. Increases for every Transmission tech evolved, unless the gene [Trans-Stasis] is used.");
-
-                RegisterSCPlusVariable(
-                    "symptom_extra_cost",
-                    "Symptom extra cost",
-                    "Additional cost of Symptom tech. Increases for every Symptom tech evolved, unless the gene [Sympto-Stasis] is used.");
-
-                RegisterSCPlusVariable(
-                    "ability_extra_cost",
-                    "Ability extra cost",
-                    "Additional cost of Ability tech. Increases for every Ability tech evolved, unless the gene [Patho-Stasis] is used.");
-
-                RegisterSCPlusVariable(
-                    "infected_this_turn",
-                    "Infected last turn",
-                    "Number of new infected humans last turn (one turn behind Daily Infections in Disease Overview).");
-
-                RegisterSCPlusVariable(
-                    "dead_this_turn",
-                    "Dead last turn",
-                    "Number of new dead humans last turn (one turn behind Daily Deaths in Disease Overview).");
-
-                RegisterSCPlusVariable(
-                    "zombies_this_turn",
-                    "Zombies last turn",
-                    "Number of new zombies last turn.");
-
-                RegisterSCPlusVariable(
-                    "infected_apes_this_turn",
-                    "Infected apes last turn",
-                    "Number of new infected apes last turn.");
-
-                RegisterSCPlusVariable(
-                    "transmission_devolve_cost",
-                    "Transmission devolve cost",
-                    "Cost penalty applied when trying to devolve Transmission tech. Increases by [Transmission devolve cost increase] on Transmission tech devolve.");
-
-                RegisterSCPlusVariable(
-                    "transmission_devolve_cost_increase",
-                    "Transmission devolve cost increase",
-                    "How much the cost penalty increases on Transmission tech devolve. Defaults to 1, or 0 if the gene [Translesion +] is used.");
-
-                RegisterSCPlusVariable(
-                    "symptom_devolve_cost",
-                    "Symptom devolve cost",
-                    "Cost penalty applied when trying to devolve Symptom tech. Increases by [Symptom devolve cost increase] on Symptom tech devolve.");
-
-                RegisterSCPlusVariable(
-                    "symptom_devolve_cost_increase",
-                    "Symptom devolve cost increase",
-                    "How much the cost penalty increases on Symptom tech devolve. Defaults to 1, or 0 if the gene [Translesion +] is used.");
-
-                RegisterSCPlusVariable(
-                    "ability_devolve_cost",
-                    "Ability devolve cost",
-                    "Cost penalty applied when trying to devolve Ability tech. Increases by [Ability devolve cost increase] on Ability tech devolve.");
-
-                RegisterSCPlusVariable(
-                    "ability_devolve_cost_increase",
-                    "Ability devolve cost increase",
-                    "How much the cost penalty increases on Ability tech devolve. Defaults to 1, or 0 if the gene [Translesion +] is used.");
-
-                RegisterSCPlusVariable(
-                    "transmission_random_mutations",
-                    "Transmission random mutations",
-                    "Whether Transmission tech can randomly mutate like Symptom tech. False by default, true if the gene [Base Oxidisation] is used.");
-
-                RegisterSCPlusVariable(
-                    "ability_random_mutations",
-                    "Ability random mutations",
-                    "Whether Ability tech can randomly mutate like Symptom tech. False by default, true if the gene [Base Hydrolysis] is used.");
-
-                RegisterSCPlusVariable(
-                    "aa_cost_modifier",
-                    "AA cost modifier",
-                    "Extra value applied to Active Ability costs. Positive values decrease cost, negative values increase. Cost cannot go below 1.");
+                LocalizationEnglish.MoreVariableTranslations();
             }
         }
 
-        internal static void RegisterSCPlusVariableFromExisting(string commonSuffix, string existingVariable = null, string namePrefix = "", string nameSuffix = "", string tooltipPrefix = "", string tooltipSuffix = "", string language = CLocalisationManager.FALLBACK_LANGUAGE)
+        internal static void RegisterSCPlusVariableFromExisting(string language, string commonSuffix, string existingVariable = null, string namePrefix = "", string nameSuffix = "", string tooltipPrefix = "", string tooltipSuffix = "")
         {
             RegisterSCPlusVariable(
+                language,
                 commonSuffix,
                 $"{namePrefix}{CLocalisationManager.GetText($"UI_Event_Variable_{existingVariable ?? commonSuffix}")}{nameSuffix}",
-                $"{tooltipPrefix}{CLocalisationManager.GetText($"Help_Event_Variable_{existingVariable ?? commonSuffix}")}{tooltipSuffix}",
-                language);
+                $"{tooltipPrefix}{CLocalisationManager.GetText($"Help_Event_Variable_{existingVariable ?? commonSuffix}")}{tooltipSuffix}");
         }
 
-        internal static void RegisterSCPlusVariable(string commonSuffix, string name, string tooltip, string language = CLocalisationManager.FALLBACK_LANGUAGE)
+        internal static void RegisterSCPlusVariable(string language, string commonSuffix, string name, string tooltip)
         {
-            RegisterVariable($"SCPlus_Event_Variable_{commonSuffix}", name, tooltip, language);
+            RegisterVariable(language, $"SCPlus_Event_Variable_{commonSuffix}", name, tooltip);
         }
 
-        internal static void RegisterSCPlusLine(string suffix, string line, string language = CLocalisationManager.FALLBACK_LANGUAGE)
+        internal static void RegisterSCPlusLine(string language, string suffix, string line)
         {
-            RegisterLine($"SCPlus_{suffix}", line, language);
+            RegisterLine(language, $"SCPlus_{suffix}", line);
         }
 
-        internal static void RegisterVariable(string commonSuffix, string name, string tooltip, string language = CLocalisationManager.FALLBACK_LANGUAGE)
+        internal static void RegisterVariable(string language, string commonSuffix, string name, string tooltip)
         {
-            RegisterLine($"UI_{commonSuffix}", $"{SCPLUS_PREFIX} {name}", language);
-            RegisterLine($"Help_{commonSuffix}", tooltip, language);
+            RegisterLine(language, $"UI_{commonSuffix}", $"{SCPLUS_PREFIX} {name}");
+            RegisterLine(language, $"Help_{commonSuffix}", tooltip);
         }
 
-        internal static void RegisterLine(string tag, string line, string language)
+        internal static void RegisterLine(string language, string tag, string line)
         {
             if (!CLocalisationManager.LanguageExists(language) || !CLocalisationManager.mpLocalisedTexts.ContainsKey(language))
             {
---
+++ patch/lang/localization/LocalizationEnglish.cs
@@ -0,0 +1,115 @@
+﻿namespace SCPlus.patch.lang.localization
+{
+    internal class LocalizationEnglish
+    {
+        private static readonly string LANGUAGE = "English";
+
+        internal static void MoreVariableTranslations()
+        {
+            LanguageRegister.RegisterSCPlusVariableFromExisting(
+                LANGUAGE,
+                "custom_global_variable_6");
+
+            LanguageRegister.RegisterSCPlusVariableFromExisting(
+                LANGUAGE,
+                "local_cure_research",
+                tooltipSuffix: $"\n{LanguageRegister.SCPLUS_PREFIX} Gets reset every turn, changes will not be kept");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "transmission_extra_cost",
+                "Transmission extra cost",
+                "Additional cost of Transmission tech. Increases for every Transmission tech evolved, unless the gene [Trans-Stasis] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "symptom_extra_cost",
+                "Symptom extra cost",
+                "Additional cost of Symptom tech. Increases for every Symptom tech evolved, unless the gene [Sympto-Stasis] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "ability_extra_cost",
+                "Ability extra cost",
+                "Additional cost of Ability tech. Increases for every Ability tech evolved, unless the gene [Patho-Stasis] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "infected_this_turn",
+                "Infected last turn",
+                "Number of new infected humans last turn (one turn behind Daily Infections in Disease Overview).");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "dead_this_turn",
+                "Dead last turn",
+                "Number of new dead humans last turn (one turn behind Daily Deaths in Disease Overview).");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "zombies_this_turn",
+                "Zombies last turn",
+                "Number of new zombies last turn.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "infected_apes_this_turn",
+                "Infected apes last turn",
+                "Number of new infected apes last turn.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "transmission_devolve_cost",
+                "Transmission devolve cost",
+                "Cost penalty applied when trying to devolve Transmission tech. Increases by [Transmission devolve cost increase] on Transmission tech devolve.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "transmission_devolve_cost_increase",
+                "Transmission devolve cost increase",
+                "How much the cost penalty increases on Transmission tech devolve. Defaults to 1, or 0 if the gene [Translesion +] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "symptom_devolve_cost",
+                "Symptom devolve cost",
+                "Cost penalty applied when trying to devolve Symptom tech. Increases by [Symptom devolve cost increase] on Symptom tech devolve.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "symptom_devolve_cost_increase",
+                "Symptom devolve cost increase",
+                "How much the cost penalty increases on Symptom tech devolve. Defaults to 1, or 0 if the gene [Translesion +] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "ability_devolve_cost",
+                "Ability devolve cost",
+                "Cost penalty applied when trying to devolve Ability tech. Increases by [Ability devolve cost increase] on Ability tech devolve.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "ability_devolve_cost_increase",
+                "Ability devolve cost increase",
+                "How much the cost penalty increases on Ability tech devolve. Defaults to 1, or 0 if the gene [Translesion +] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "transmission_random_mutations",
+                "Transmission random mutations",
+                "Whether Transmission tech can randomly mutate like Symptom tech. False by default, true if the gene [Base Oxidisation] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "ability_random_mutations",
+                "Ability random mutations",
+                "Whether Ability tech can randomly mutate like Symptom tech. False by default, true if the gene [Base Hydrolysis] is used.");
+
+            LanguageRegister.RegisterSCPlusVariable(
+                LANGUAGE,
+                "aa_cost_modifier",
+                "AA cost modifier",
+                "Extra value applied to Active Ability costs. Positive values decrease cost, negative values increase. Cost cannot go below 1.");
+        }
+    }
+}
