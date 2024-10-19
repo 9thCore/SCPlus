74a9c0819d91304fad871212ce07b1e3022852e2
9thCore <38700349+9thCore@users.noreply.github.com>
2024-10-19 12:22:49 +0300

    Redirector component
    Redirects the target of a VariableSetter to another object

---
+++ patch/game_event/VariableSetterRedirector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace SCPlus.patch.game_event
+{
+    internal class VariableSetterRedirector : MonoBehaviour
+    {
+        // Accepts the original parameter and returns a new object, which is the "redirected" object
+        // Can return same object if no redirection is required
+        internal Func<object, object> apply = null;
+
+        public object Apply(object value)
+        {
+            if (apply == null)
+            {
+                return value;
+            }
+
+            try
+            {
+                return apply(value);
+            }
+            catch (Exception ex)
+            {
+                Plugin.Logger.LogError($"Encountered error while redirecting: {ex}");
+                return value;
+            }
+        }
+    }
+}
