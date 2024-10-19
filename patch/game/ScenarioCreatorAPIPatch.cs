4a3c65f78f9844611252c5b20579fd3c12b240e9
9thCore <38700349+9thCore@users.noreply.github.com>
2024-10-19 12:46:15 +0300

    Data clear patch
    Patch the clearing of scenario information to also clear extra data

---
+++ patch/game/ScenarioCreatorAPIPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using SCPlus.patch.game_event;
+using SCPlus.plugin;
+
+namespace SCPlus.patch.game
+{
+    [HarmonyPatch(typeof(ScenarioCreatorAPI), nameof(ScenarioCreatorAPI.ClearScenarioInformation))]
+    class ScenarioCreatorAPIPatch
+    {
+        private static void Postfix()
+        {
+            if (!Config.expandEventFunctionality.Value)
+            {
+                return;
+            }
+
+            ExtraFunctionality.eventData.Clear();
+        }
+    }
+}
