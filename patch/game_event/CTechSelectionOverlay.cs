f5f61fc4f0b8ca0959ecc62bb0b4f9efee083350
9thCore <38700349+9thCore@users.noreply.github.com>
2024-10-19 12:38:37 +0300

    CTechSelectionOverlay component
    Inherits from CTechTriggerOverlay, is but a more generalised version of it

---
+++ patch/game_event/CTechSelectionOverlay.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using SCPlus.patch.hierarchy;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SCPlus.patch.game_event
+{
+    internal class CTechSelectionOverlay : CTechTriggerOverlay
+    {
+        public string tooltipPositiveText = "";
+        public string tooltipNegativeText = "";
+
+        public override void ClickedApply()
+        {
+            CUIManager.instance.HideOverlay(this);
+            EventHelper.eventScreen.SaveCurrentEvent();
+        }
+
+        public override void ClickedClose()
+        {
+            CUIManager.instance.HideOverlay(this);
+        }
+
+        public override void NewTrigger(bool isTriggered, UITable table, ParameterCondition root, List<TriggerList> list)
+        {
+            base.NewTrigger(isTriggered, table, root, list);
+
+            TriggerList component = list.Last();
+            if (component == null)
+            {
+                Plugin.Logger.LogError($"Invalid: no last {nameof(TriggerList)} in list");
+                return;
+            }
+            EditDescription(component, isTriggered);
+        }
+
+        public override void ExistingTrigger(bool isTriggered, UITable table, ParameterCondition cond, List<TriggerList> list)
+        {
+            base.ExistingTrigger(isTriggered, table, cond, list);
+
+            TriggerList component = list.Last();
+            if (component == null)
+            {
+                Plugin.Logger.LogError($"Invalid: no last {nameof(TriggerList)} in list");
+                return;
+            }
+            EditDescription(component, isTriggered);
+        }
+
+        private void EditDescription(TriggerList component, bool isPositive)
+        {
+            if (!HierarchyHelper.TryFindComponentWithLogging(component.transform, out TooltipObject tooltip))
+            {
+                Plugin.Logger.LogError($"Could not edit description of {component}");
+                return;
+            }
+
+            tooltip.localisationTag = isPositive ? tooltipPositiveText : tooltipNegativeText;
+        }
+    }
+}
