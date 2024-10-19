2866df17382ceef94521aa2b48a83a0b0ba2f3e4
9thCore <38700349+9thCore@users.noreply.github.com>
2024-10-19 12:23:09 +0300

    Patch VariableSetter
    Use Redirector, if it exists

---
+++ patch/game_event/VariableSetterPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using SCPlus.plugin;
+
+namespace SCPlus.patch.game_event
+{
+    [HarmonyPatch(typeof(VariableSetter))]
+    internal class VariableSetterPatch
+    {
+        // There are too many subclasses I should extend to redirect this "normally",
+        // so patching imma do
+        [HarmonyPatch(nameof(VariableSetter.Initialise))]
+        [HarmonyPrefix]
+        private static void AccessValuePatch(VariableSetter __instance, ref object to)
+        {
+            if (!Config.expandEventFunctionality.Value)
+            {
+                return;
+            }
+
+            if (__instance.TryGetComponent(out VariableSetterRedirector redirector))
+            {
+                to = redirector.Apply(to);
+            }
+        }
+    }
+}
