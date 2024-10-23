b8e71919c02b8e6f6d2617240b89a8eccb132824
9thCore <38700349+9thCore@users.noreply.github.com>
2024-10-23 20:43:48 +0300

    hotfix: clear data

---
+++ patch/game_event/per_country/PerCountryFunctionality.ClearScenarioInformationPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using SCPlus.plugin;
+
+namespace SCPlus.patch.game_event.per_country
+{
+    internal partial class PerCountryFunctionality
+    {
+        [HarmonyPatch(typeof(ScenarioCreatorAPI), nameof(ScenarioCreatorAPI.ClearScenarioInformation))]
+        private static class ClearScenarioInformationPatch
+        {
+            private static void Postfix()
+            {
+                if (!Config.perCountryEventFunctionality.Value)
+                {
+                    return;
+                }
+
+                perCountryEvents.Clear();
+                subEvents.Clear();
+                creatingPerCountryEvent = false;
+            }
+        }
+    }
+}
