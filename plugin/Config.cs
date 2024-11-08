05b594ce7d1d9f524abe0c6d2ddbff55a18a1475
9thCore <38700349+9thCore@users.noreply.github.com>
2024-11-08 15:22:29 +0200

    amend config desc
    as it turns out, most things arent compatible, actually! it'll end up being a global thing

--- plugin/Config.cs
+++ plugin/Config.cs
@@ -52,7 +52,7 @@ namespace SCPlus.plugin
                 "Event",
                 nameof(perCountryEventFunctionality),
                 false,
-                "Allow creation of a special \"per-country\" event type, which will run for every country.\n/!\\ Not compatible with mobile clients. /!\\");
+                "Allow creation of a special \"per-country\" event type, which will run for every country.");
 
             expandTraitFunctionality = config.Bind(
                 "Trait",
